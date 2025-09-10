63ddb85c23124ec91ee9ce778768536c40976665
max del real <maxadelreal@csu.fullerton.edu>
2025-09-09 21:04:31 -0700

    changed secret key

--- wasted_app/__init__.py
+++ wasted_app/__init__.py
@@ -3,7 +3,7 @@ from flask import Flask
 
 def create_app():
     app = Flask(__name__)
-    app.config['SECRET_KEY'] = 'your-secret-key-here'
+    app.config['SECRET_KEY'] = 'passkeywaste'
     
     # Import and register routes
     from wasted_app.routes import main
