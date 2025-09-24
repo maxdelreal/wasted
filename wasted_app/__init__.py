67240d99d305fe00020e916d113c46730ef34d19
rebecca gomez <gomez.rebecca253@csu.fullerton.edu>
2025-09-24 09:16:03 -0700

    sql file working on

--- wasted_app/__init__.py
+++ wasted_app/__init__.py
@@ -1,8 +1,11 @@
 # turns folder into a python package
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
 
 #Creating database w SQLAlchemy // remember to install sqalchemy through pip "pip install flask-sqlalchemy flask-migrate"
 db = SQLAlchemy()
+
 
 def create_app():
     app = Flask(__name__)
