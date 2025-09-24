03fcbf7a4259a421e751ed0144f1e5e79adc9477
max del real <100966155+maxdelreal@users.noreply.github.com>
2025-09-24 08:59:47 -0700

    Update __init__.py w Alchemy configuration and changed secretkey

--- wasted_app/__init__.py
+++ wasted_app/__init__.py
@@ -1,9 +1,19 @@
 # turns folder into a python package
 from flask import Flask
 
+#Creating database w SQLAlchemy // remember to install sqalchemy through pip "pip install flask-sqlalchemy flask-migrate"
+db = SQLAlchemy()
+
 def create_app():
     app = Flask(__name__)
-    app.config['SECRET_KEY'] = 'your-secret-key-here'
+    app.config['SECRET_KEY'] = 'wastedkey'
+
+    #db config
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+    #DB initialization
+    db.init_app(app)
     
     # Import and register routes
     from wasted_app.routes import main
