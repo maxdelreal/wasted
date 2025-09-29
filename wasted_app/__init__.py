9176b2012b294559a9afea4174ec73086cd57a80
max del real <100966155+maxdelreal@users.noreply.github.com>
2025-09-29 23:16:21 +0000

    modified init py to setup user logins

--- wasted_app/__init__.py
+++ wasted_app/__init__.py
@@ -1,9 +1,11 @@
 # turns folder into a python package
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import 
 
 #Creating database w SQLAlchemy // remember to install sqalchemy through pip "pip install flask-sqlalchemy flask-migrate"
 db = SQLAlchemy()
+login_manager = LoginManager()
 
 
 def create_app():
@@ -16,12 +18,22 @@ def create_app():
 
     #DB initialization
     db.init_app(app)
+
+    # Initialize login manager
+    login_manager.init_app(app)
+    login_manager.login_view = 'main.login' # directs user here if not logged in
     
     # Import and register routes
     from wasted_app.routes import main
     app.register_blueprint(main)
     
     return app
+
+# Load user function 
+@login_manager.user_loader
+def load_user(user_id):
+    from wasted_app.models import User
+    return User.query.get(int(user_id))
 
 # For direct import
 app = create_app()
